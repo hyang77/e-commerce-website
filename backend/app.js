d13308c4880574787793aae012b864a3be9f5694
Haoli Yang <hyang77@hawk.iit.edu>
2020-06-06 15:34:05 +0800

    convert to es6 module

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,30 @@
 // =============    DECLARE VARIABLES   =============
-const   express     = require('express'),
-        app         = express(),
-        connectDB   = require('./Database/connections'),
-        port        = process.env.PORT ||2355;
+import express from "express";
+import connectDB from "./Database/connections";
+const app = express();
+const port = process.env.PORT || 2355;
+/**
+ * const express = require("express"),
+  app = express(),
+  connectDB = require("./Database/connections"),
+  port = process.env.PORT || 2355;
+ */
 
 // Using stylesheet css in public directory
-app.use(express.static(__dirname + "/public"))
-// 
+app.use(express.static(__dirname + "/public"));
+//
 app.use(express.json());
 
 // Connect to MongoDB Atlas
 connectDB();
 
 // ROUTES
-app.get('/', (req, res) =>{
-    res.render('home')
-})
-    
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+
 // Listening to the server
-app.listen(port, () =>{
-    console.log(`Server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
